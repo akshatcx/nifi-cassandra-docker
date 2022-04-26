cce542b987b58cd787428321425a99d2d4de6596
akshatcx <akshat.c2k@gmail.com>
2022-04-26 12:19:27 +0000

    add: sample files

--- scripts/createTable.py
+++ scripts/createTable.py
@@ -3,18 +3,36 @@ import pandas as pd
 import sys
 file = pd.read_csv(sys.stdin)
 
+KEYSPACE = 'dfs'
 # file = pd.read_csv("./monthly_data1.csv")
-command = "CREATE TABLE data.precipdataa ("
+
+maxUniqueNumber = 0
+maxUniqueColumn = ""
+for column in file:
+    if(file[column].nunique()>maxUniqueNumber):
+        maxUniqueNumber = file[column].nunique()
+        maxUniqueColumn = column
+
+pandasDataTypeToCassandraDataType = {
+    'float64':'float',
+    'string':'text',
+    'date':'timestamp',
+    'object' : 'text'
+}
+
+command = f"CREATE TABLE IF NOT EXISTS {KEYSPACE}."+str(sys.argv[1]).split('.')[0]+"  ("
 for i in file.columns:
-    command = command + str(i) + " " + "text" + ","
-command = command + "PRIMARY KEY (" + "date" +  "));"
+    command = command + str(i) + " " + pandasDataTypeToCassandraDataType[str(file.dtypes[i])] + ","
+command = command + "PRIMARY KEY (" + maxUniqueColumn +  "));"
+
+
 print(command)
 
 # Todo
 # name
 # primary key
-# data type 
-# keyspace 
+# data type
+# keyspace
 
 
 # CREATE TABLE data.precipdataa (date date, precip float, PRIMARY  KEY (date));
