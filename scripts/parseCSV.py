b9ef3bf00d38ea59f415b5f54e5ee8915ff71759
akshatcx <akshat.c2k@gmail.com>
2022-04-26 21:12:00 +0530

    fix: scripts

---
+++ scripts/parseCSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+import pandas as pd
+
+KEYSPACE = 'dfs'
+DTYPES = {
+    'float64': 'float',
+    'string': 'text',
+    'date': 'timestamp',
+    'object' : 'text'
+}
+
+# df = pd.read_csv("../sample_files/monthly_data.csv")
+df = pd.read_csv(sys.stdin)
+tableName = str(sys.argv[1]).split('.')[0]
+
+maxUniqueNumber = 0
+maxUniqueColumn = ""
+
+for column in df:
+    if df[column].nunique() > maxUniqueNumber:
+        maxUniqueNumber = df[column].nunique()
+        maxUniqueColumn = column
+
+# create table
+print(f"CREATE TABLE IF NOT EXISTS {KEYSPACE}.{tableName} ({', '.join([col + ' ' + DTYPES[str(df.dtypes[col])] for col in df.columns])}, PRIMARY KEY ({maxUniqueColumn}));")
+
+# insert rows
+# INSERT INTO dfs.dummy_data (date , precip) VALUES ('1893-01-01' , 1.32);
+def get_values(row):
+    values = []
+    for col in df.columns:
+        if DTYPES[str(df.dtypes[col])] == 'text' or DTYPES[str(df.dtypes[col])] == 'timestamp': values.append(f"'{str(row[col])}'")
+        else: values.append(str(row[col]))
+    return values
+
+for i, row in df.iterrows():
+    print(f"INSERT INTO {KEYSPACE}.{tableName} ({', '.join(df.columns)}) VALUES ({', '.join(get_values(row))});")
---
+++ scripts/parseJSON.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import sys
+import pandas as pd
+
+KEYSPACE = 'dfs'
+DTYPES = {
+    'float64': 'float',
+    'string': 'text',
+    'datetime64[ns]': 'timestamp',
+    'object' : 'text'
+}
+
+# df = pd.read_json("../sample_files/monthly_data.json")
+df = pd.read_json(sys.stdin)
+tableName = str(sys.argv[1]).split('.')[0]
+
+maxUniqueNumber = 0
+maxUniqueColumn = ""
+
+for column in df:
+    if df[column].nunique() > maxUniqueNumber:
+        maxUniqueNumber = df[column].nunique()
+        maxUniqueColumn = column
+
+# create table
+print([str(df.dtypes[col]) for col in df.columns])
+print(f"CREATE TABLE IF NOT EXISTS {KEYSPACE}.{tableName} ({', '.join([col + ' ' + DTYPES[str(df.dtypes[col])] for col in df.columns])}, PRIMARY KEY ({maxUniqueColumn}));")
+
+# insert rows
+# INSERT INTO dfs.dummy_data (date , precip) VALUES ('1893-01-01' , 1.32);
+def get_values(row):
+    values = []
+    for col in df.columns:
+        if DTYPES[str(df.dtypes[col])] == 'text' or DTYPES[str(df.dtypes[col])] == 'timestamp': values.append(f"'{str(row[col])}'")
+        else: values.append(str(row[col]))
+    return values
+
+for i, row in df.iterrows():
+    print(f"INSERT INTO {KEYSPACE}.{tableName} ({', '.join(df.columns)}) VALUES ({', '.join(get_values(row))});")
